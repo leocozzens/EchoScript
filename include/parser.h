8aacfdb5de7189614359e93239153d576896748a
Leo Cozzens <leo@narthexnetworks.com>
2024-01-27 17:23:12 -0600

    Reset
    Wiped project

--- include/parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include <tokenizer.h>
-
-TokenData *run_pipeline(char *data, size_t fSize);
-
-#endif
--- src/parser.c
+++
@@ -1,9 +0,0 @@
-// Local headers
-#include <parser.h>
-#include <tokenizer.h>
-
-TokenData *run_pipeline(char *data, size_t fSize) { // TODO: When procedures become more complex parse operations into AST
-    TokenData *tokenHead = tokenize_file(data, fSize);
-    // Render tokens into AST
-    return tokenHead;
-}
